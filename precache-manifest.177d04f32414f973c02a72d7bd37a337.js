c6073eb352cbcee2c61b612366e81bb12688cbd0
Anton Tipunov <122938503+anton-tipunov@users.noreply.github.com>
2023-01-18 11:13:59 +0100

    Initial

---
+++ precache-manifest.177d04f32414f973c02a72d7bd37a337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54d7d27c9c6e8ef118e8",
+    "url": "/static/css/main.5bebc5b3.chunk.css"
+  },
+  {
+    "revision": "54d7d27c9c6e8ef118e8",
+    "url": "/static/js/main.54d7d27c.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "3b5030f2043b864932c7",
+    "url": "/static/js/2.3b5030f2.chunk.js"
+  },
+  {
+    "revision": "1ed698c15cd075133fed",
+    "url": "/static/js/runtime~main.1ed698c1.js"
+  },
+  {
+    "revision": "da3243109ccfaebdf5adc5b5d7c52682",
+    "url": "/index.html"
+  }
+];
